64e8257731d28ac0d8bfe37118da128c0ed49457
degraffa <ard96@cornell.edu>
2019-10-04 20:01:07 -0400

    Sphere intersection attempted

--- ray/src/ray1/surface/Sphere.java
+++ ray/src/ray1/surface/Sphere.java
@@ -1,5 +1,6 @@
 package ray1.surface;
 
+import egl.math.Vector2d;
 import ray1.IntersectionRecord;
 import ray1.Ray;
 import egl.math.Vector3;
@@ -31,15 +32,56 @@ public class Sphere extends Surface {
    * outRecord is not modified.
    *
    * @param outRecord the output IntersectionRecord
-   * @param ray the ray to intersect
+   * @param rayIn the ray to intersect
    * @return true if the surface intersects the ray
    */
   public boolean intersect(IntersectionRecord outRecord, Ray rayIn) {
     // TODO#Ray Task 2: fill in this function.
+        // Look at textbook chapter 4.4.1 for the derivation of this
+        // we want to find the point on the sphere at which we intersect with the sphere
+        // we can get this by finding t in the equation r(t) = p + td, where p is the origin of RayIn and d is the direction
 
-	    
+        double negativeB = -rayIn.direction.dot(rayIn.origin.clone().sub(center));
+        double bSquared = negativeB * negativeB;
+        Vector3d originMinusCenter = rayIn.origin.clone().sub(center);
+        double dirDotDir = rayIn.direction.dot(rayIn.direction);
+
+        double A = dirDotDir;
+        double B = -negativeB;
+        double C = originMinusCenter.dot(originMinusCenter) - radius*radius;
+
+        // if the determinant is negative, return false
+        double determinant = B*B - A*C;
+        if (determinant < 0) return false;
+
+        double determinantSqrt = Math.sqrt(determinant);
+        double tPos = (negativeB + determinantSqrt) / dirDotDir;
+        double tNeg = (negativeB - determinantSqrt) / dirDotDir;
+
+        double t = (determinant != 0) ?  tNeg : negativeB / dirDotDir;
+
+        if (t < rayIn.start || t > rayIn.end) return false;
+
 	    // If there was an intersection, fill out the intersection record
+        Vector3d location = new Vector3d(rayIn.origin.clone().add(rayIn.direction.clone().mul(t)));
+        Vector3d normal = location.clone().sub(center);
 
+        //Calculate uv by using spherical coordinates and the last assignment's method of calculation
+        // horizontal angle, starting at 0 on positive x axis
+        double theta = Math.acos(location.z / radius) * 2;
+        // Vertical angle, starting at 0 on position y axis
+        double phi = Math.atan(location.y / location.x) * 2;
+        // u is basically just a percentage of a circle in the xz plane that has been completed so far
+        double u = theta / (2 * Math.PI);
+        // v is the percentage of the vertical distances that have been completed so far, starting at 1 going to 0
+        double v = (location.z == -1) ? 0 : 1 - phi / Math.PI;
+        Vector2d texCoords = new Vector2d(u, v);
+
+        outRecord.location.set(location);
+        outRecord.texCoords.set(texCoords);
+        outRecord.normal.set(normal);
+        outRecord.t = t;
+        outRecord.surface = this;
 	    
 	    return false;
   }
